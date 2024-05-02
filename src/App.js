99b16a9bdbe51c307b396ebeeb90827512a19e00
jason-roesgen <jason.roesgen@gmail.com>
2024-05-02 21:03:12 +0200

    remove comments+console.logs

--- src/App.js
+++ src/App.js
@@ -4,27 +4,15 @@ import TextField from '@mui/material/TextField';
 import CalculatorButtons from './CalculatorButtons'; 
 
 function App() {
-  // use useState of react ->
-  // input and setInput
-  // result and setResult
-  //
-  // add somehow the different values to those states
-
   const [input, setInput] = useState('')
-  const [result, setResult] = useState('')
   const handleButtonClick = (label) => {
 
     // if number clicked, append to input 
     if (!isNaN(label)) {
       const newInput = input + label;
       setInput(newInput);
-
-      console.log("input", input); // previous clicked number 
-      console.log("newInput", newInput); // clicked number 
-      console.log("result", result); // logs nothing 
-      console.log("setResult", setResult); // ion know
-    } else {
-      alert("i see u :icu:");
+} else {
+      alert("Fuktion noch nicht verfügbar");
     };
 
     // if operator clicked, update to input to include the operator 
@@ -39,7 +27,6 @@ function App() {
   return (
     <div className="container">
       <TextField className="display" id="outlined-basic" variant="outlined" value={input}/>
-      {/* <div className='display'>{input}</div> */}
       <CalculatorButtons onButtonClick={handleButtonClick} />
     </div>
   );
