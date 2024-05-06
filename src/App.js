a15e1673547991242c0c4d2ffbbd81c975d7e1f6
Jason Rösgen <jason.roesgen@gmail.com>
2024-05-06 23:58:00 +0200

    format code with prettier

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { useState } from 'react';
-import './App.css';
-import TextField from '@mui/material/TextField';
-import CalculatorButtons from './CalculatorButtons'; 
+import React, { useState } from "react";
+import "./App.css";
+import TextField from "@mui/material/TextField";
+import CalculatorButtons from "./CalculatorButtons";
 
 function App() {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState("");
   const handleButtonClick = (label) => {
-
-    // if number clicked, append to input 
+    // if number clicked, append to input
     if (!isNaN(label)) {
       setInput((prevInput) => prevInput + label);
     } else if (label === "AC") {
-      setInput('');
+      setInput("");
       console.log("input: ", input);
-    } else if (label === "C" ) {
-        setInput((prevInput) => prevInput.slice(0, -1));
-      } else if (['+', '-', '*', '/'].includes(label)) {
-      // if operator clicked, update to input to include the operator 
-      if (!['+', '-', '*', '/'].includes(input.slice(-1))) {
+    } else if (label === "C") {
+      setInput((prevInput) => prevInput.slice(0, -1));
+    } else if (["+", "-", "*", "/"].includes(label)) {
+      // if operator clicked, update to input to include the operator
+      if (!["+", "-", "*", "/"].includes(input.slice(-1))) {
         setInput((prevInput) => prevInput + label);
       }
     } else if (label === "=") {
-      // if = button clicked, evaluate expression in input and update result 
+      // if = button clicked, evaluate expression in input and update result
       try {
         const result = eval(input);
         if (!isFinite(result)) {
@@ -32,16 +31,22 @@ function App() {
       } catch (error) {
         alert(error.message);
       }
-
     } else {
       alert("Funktion noch nicht verfügbar!");
-    };
+    }
   };
 
   return (
     <div className="container">
-      <TextField className="display" id="outlined-basic" variant="outlined" value={input}/>
-      <CalculatorButtons onButtonClick={handleButtonClick} />
+      <TextField
+        className="display"
+        id="outlined-basic"
+        variant="outlined"
+        value={input}
+      />
+      <div className="button-frame">
+        <CalculatorButtons onButtonClick={handleButtonClick} />
+      </div>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Button.css'; // Import CSS for styling
+import React from "react";
+import "./Button.css"; // Import CSS for styling
 
 const Button = ({ label, onClick }) => {
   return (
